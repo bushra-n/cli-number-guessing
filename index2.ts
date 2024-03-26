aff6e108484903ce01302f3fa7dd9b50d4469170
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-03-26 15:18:17 +0500

    Add files via upload

---
+++ index2.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+// computer will generate a random number
+// user input for guessing number
+// compare user input with computer generated number and show reults
+const randomNumber=Math.floor(Math.random()*10+1);
+const answers= await inquirer.prompt([
+    { name:"userGuessedNumber",
+    type:"number",
+    message:"Please guess a number:",
+},
+])
+if (answers.userGuessedNumber===randomNumber)
+{console.log("congratulation!you guessed right number.");
+}else{
+    console.log("you guessed wrong number");
+}
